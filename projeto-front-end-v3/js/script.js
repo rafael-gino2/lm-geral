b0eaa68db3be98b12f80d7342455215b1c92a3c2
rafael-gino2 <rafaelgino27@outlook.com>
2023-05-23 11:40:04 -0300

    .

---
+++ projeto-front-end-v3/js/script.js
@@ -0,0 +1,19 @@
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = () => {
+//alert("CLICOU!");
+
+if (darkmode.classList.contains("bx-moon")) {
+    //alert("MODO ESCURO INATIVO");
+    darkmode.classList.replace('bx-moon', 'bx-sun')
+    document.body.classList.add('color');
+
+    }else{
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('color');
+    }
+
+
+    
+}
+
